477c355a951c1eaf1bf98d03b49748cac64b9105
cheetah676 <league@WTS22.attlocal.net>
2018-01-20 16:04:29 -0800

    birthday reminder, high low game, nasa countdown, stars how

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -9,23 +9,38 @@ import javax.swing.JOptionPane;
 public class BirthdayReminder {
 
 	public static void main(String[] args) {
-	
+
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "March 16th";
+		String dadsBirthday = "March 26th";
+		String myBirthday = "January 1st";
+		String sistersBirthday = "August 21st";
 
-		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		// 2. Find out which birthday the user wants and and store their response in a
+		// variable
+		String birthday = JOptionPane.showInputDialog("What birthday do you want? Mom, Dad, Sophia, or Izi?");
 		// 3. Print out what the user typed
-		
+		System.out.println(birthday);
 		// 4. if user asked for "mom"
-			//print mom's birthday
+		// print mom's birthday
+		if (birthday.equals("Mom")) {
+			System.out.println(momsBirthday);
+		}
 		// 5. if user asked for "dad"
-			// print dad's birthday
+		// print dad's birthday
+		else if (birthday.equals("Dad")) {
+			System.out.println(dadsBirthday);
+		}
 		// 6. if user asked for your name
-			// print myBirthday
-		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
-	} 
+		// print myBirthday
+		else if (birthday.equals("Izi")) {
+			System.out.println(myBirthday);
+		} else if (birthday.equals("Sophia")) {
+			System.out.println(sistersBirthday);
+		}
+		// 7. otherwise print "Sorry, i don't remember that person's birthday!"
+		else {
+			System.out.println("Sorry, I don't remember that person's birthday!");
+		}
+	}
 }
--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,29 +5,40 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		// 3. Change this line to give you a random number between 1 - 100.
+		int random = new Random().nextInt(100);
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. do the following 10 times
-		
-			// 1. ask the user for a guess using a pop-up window, and save their response 
+		for (int i = 0; i < 10; i++) {
 
+			// 1. ask the user for a guess using a pop-up window, and save their response
+			String guess = JOptionPane.showInputDialog("What's your guess?");
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int numberguess = Integer.parseInt(guess);
 			// 5. if the guess is correct
-				// 6. win
+			if (numberguess == random) {
+				JOptionPane.showMessageDialog(null, "You win!");
+			}
+			// 6. win
 			// 7. if the guess is high
-				// 8. tell them it's too high
+			if (numberguess > random) {
+				JOptionPane.showMessageDialog(null, "Too high!");
+			}
+			// 8. tell them it's too high
 			// 9. if the guess is low
-				// 10. tell them it's too low
-
+			if (numberguess < random) {
+				JOptionPane.showMessageDialog(null, "Too low!");
+			}
+			// 10. tell them it's too low
+		}
 		// 12. tell them they lose
+		JOptionPane.showMessageDialog(null, "You lose!");
 	}
 
 }
-
-
